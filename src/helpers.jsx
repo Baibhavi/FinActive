7b7f3b0549ae2a95d9d5adb20b917238e3f80e22
Baibhavi <baibhavipandey@gmail.com>
2024-07-30 17:20:41 +0530

    expense form created

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -1,3 +1,7 @@
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 2000))
+
+
+// colors
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.length ?? 0;
   return `${existingBudgetLength * 34} 65% 50%`
@@ -23,6 +27,23 @@ export const createBudget = ({
   return localStorage.setItem("budgets",
     JSON.stringify([...existingBudgets, newItem]))
 }
+
+// create expense
+export const createExpense = ({
+  name, amount, budgetId
+}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId
+  }
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem("expenses",
+    JSON.stringify([...existingExpenses, newItem]))
+}
+
 
 // delete item
 export const deleteItem = ({ key }) => {
